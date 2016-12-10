944e1e7324bb023cc6924d369ff115f5ab45c987
André Schnabel <andreschnabel@me.com>
2016-12-10 18:38:45 +0100

    First work on psplibconverter with tests

---
+++ psplibconverter.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+import json
+import unittest
+
+
+def show_usage():
+    print('Wrong number of arguments!\n' +
+          'Usage: ./psplibconverter.py smfilename')
+
+
+def extract_val(lines, line_prefix, exception_msg, sep=':'):
+    for line in lines:
+        if line.startswith(line_prefix):
+            return int(re.sub('\D', '', line.split(sep)[1]))
+    raise Exception(exception_msg)
+
+
+def parse_lines(lines):
+    njobs = extract_val(lines, 'jobs (incl. supersource/sink', 'Unable to extract number of jobs!')
+    nperiods = extract_val(lines, 'horizon', 'Unable to extract number of time periods!')
+    nres = extract_val(lines, ' - renewable', 'Unable to extract number of resources!')
+    data = {}
+    return data
+
+
+def write_data_to_gams_file(data, fn):
+    with open(fn, 'w') as fp:
+        # TODO: Implement me!
+        # Also call GAMS for gdx conversion on this gams file!
+        pass
+
+
+def convert_file(smfilename):
+    instname = os.path.splitext(os.path.basename(smfilename))[0]
+    with open(smfilename, 'r') as fp:
+        lines = fp.readlines()
+        data = parse_lines(lines)
+        with open(instname + '.json', 'w') as jsonfp:
+            jsonfp.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+        write_data_to_gams_file(data, instname+'.gms')
+
+
+EXAMPLE_PROJECT = '''
+************************************************************************
+file with basedata            : ..\EXAMPLE\EXPL.BAS
+initial value random generator: 12164
+************************************************************************
+projects                      :  1
+jobs (incl. supersource/sink ):  12
+horizon                       :  20
+RESOURCES
+  - renewable                 :  1   R
+  - nonrenewable              :  0   N
+  - doubly constrained        :  0   D
+************************************************************************
+PROJECT INFORMATION:
+pronr.  #jobs rel.date duedate tardcost  MPM-Time
+    1     12       0       33        1       33
+************************************************************************
+PRECEDENCE RELATIONS:
+jobnr.    #modes  #successors   successors
+   1        1          2           2   6
+   2        1          1           3
+   3        1          2           4   8
+   4        1          1           5
+   5        1          1           10
+   6        1          1           7
+   7        1          2           4   8
+   8        1          1           9
+   9        1          2          10   11
+  10        1          1          12
+  11        1          1          12
+  12        1          0
+************************************************************************
+REQUESTS/DURATIONS:
+jobnr. mode duration  R 1
+------------------------------------------------------------------------
+  1      1     0       0
+  2      1     2       3
+  3      1     1       3
+  4      1     2       3
+  5      1     3       3
+  6      1     2       2
+  7      1     1       2
+  8      1     2       2
+  9      1     2       1
+ 10      1     1       1
+ 11      1     2       1
+ 12      1     0       0
+************************************************************************
+RESOURCEAVAILABILITIES:
+  R 1
+    3
+************************************************************************
+'''
+
+
+class TestConverter(unittest.TestCase):
+    def test_parse_lines(self):
+        data = parse_lines(EXAMPLE_PROJECT.split('\n'))
+        self.assertEqual([0, 2, 1, 2, 3, 2, 1, 2, 2, 1, 2, 0], data['durations'])
+        self.assertEqual([[0, 3, 3, 3, 3, 2, 2, 2, 1, 1, 1, 0]], data['demands'])
+        self.assertEqual([3], data['capacities'])
+        self.assertEqual([[0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                          [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], data['adjacencyMatrix'])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        show_usage()
+        unittest.main()
+    else:
+        convert_file(sys.argv[1])
